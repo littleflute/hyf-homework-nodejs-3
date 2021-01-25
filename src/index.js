0c2c71b291db862650e9bf98f12ea0bc88860c33
littleflute <littleflute@163.com>
2021-01-25 15:28:35 -0800

    step-2+

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/users',function(req,res){
+	console.log("step-2");
+	res.json([]);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
