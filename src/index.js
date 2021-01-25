8ec6883dc0276a60b05ee60ff5340545b75c8743
littleflute <littleflute@163.com>
2021-01-25 15:55:51 -0800

    o.id=0;

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/users',function(req,res){
-	console.log("step-2");
+	console.log("xdtest: step-2");
 	res.json([]);
 })
 
 // CREATE
 app.post('/users',(req,res)=>{
     var o = {};
-    o.name = "xd";
+    o.id = 0;
     res.json({success: true,data: o})
   })
 
